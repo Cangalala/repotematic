87b28ae7079cc19673bd307186f53ca7b430103f
Cangalala <103964892+Cangalala@users.noreply.github.com>
2022-11-28 19:49:04 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
 import os
 import ProxyCloud
 
-BOT_TOKEN = '5898911288:AAEcGULhQKOkkkpDRCJU4ClMyImRRR1n-KM' #Aqui va el token del bot
-API_ID =  9902519 #Tu api id de telegram
-API_HASH = '9d8097d05bbc90a6ed2a7a81abcd4e8a' #Tu api id de telegram
+BOT_TOKEN = '5620117342:AAGZ0cTnmVuW9GQPW6UVxSQ9ZaE0QiB1W2w' #Aqui va el token del bot
+API_ID = 15745491 #Tu api id de telegram
+API_HASH = '2618ec2cdeb1b513eabf02f83f5a1553' #Tu api id de telegram
 SPLIT_FILE = 1024 * 1024 * int(os.environ.get('split_file','99'))
 ROOT_PATH = 'root/'
-ACCES_USERS = os.environ.get('tl_admin_user','Abolanos3').split(';')
+ACCES_USERS = os.environ.get('tl_admin_user','overlordox').split(';')
 
 static_proxy = 'http://KDDHKIYEJEJGGIYFJEGDIEYEKIKGFFRJHHCDHICJ'
 PROXY = ProxyCloud.parse(static_proxy)
@@ -20,5 +20,5 @@ if PROXY:
 #ponerlos valor 0 siempre
 
 space = {}
-space['Abolanos3'] = 0
+space['overlordox'] = 0
 
